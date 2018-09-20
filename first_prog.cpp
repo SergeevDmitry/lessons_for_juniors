a1cacfcb5848173b8adb8bef8da3c7904184203b
sergeevdms <sergeevdms@gmail.com>
2018-09-21 01:03:23 +0300

    added FP

---
+++ first_prog.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main(){
+	// Change console character set
+	system("chcp 1251>nul");
+
+	// Display message in cosnole
+	count<<"Coding on C++!"<<endl;
+
+	// Timeout for console window
+	system("pause>nul");
+	return 0;
+}
